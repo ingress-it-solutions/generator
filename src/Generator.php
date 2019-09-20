57f63828a2c559f40c1cca0b7a42d8275efb37b0
Bhavik (MM Ingress IT Solutions) <bhavik@ingressit.com>
2019-09-20 13:12:05 +0530

    Updated with working code for LM

--- src/Generator.php
+++ src/Generator.php
@@ -6,6 +6,14 @@ use IngressITSolutions\Generator\Exception\BaseException;
 
 class Generator
 {
+
+    //Block size for encryption block cipher
+    //private $ENCRYPT_BLOCK_SIZE = 200;// this for 2048 bit key for example, leaving some room
+
+    //Block size for decryption block cipher
+    //private $DECRYPT_BLOCK_SIZE = 256;// this again for 2048 bit key
+
+
     /**
      * Generate license file.
      *
@@ -17,21 +25,29 @@ class Generator
      */
     public static function generate($data, $privateKey)
     {
+        $encrypted = '';
+        $plainData = json_encode($data);
+        $plainData = str_split($plainData, 200);
+
         $key = openssl_pkey_get_private($privateKey);
         if (!$key) {
             throw new BaseException("OpenSSL: Unable to get private key");
         }
 
-        $success = openssl_private_encrypt(json_encode($data), $signature, $key);
-        openssl_free_key($key);
+        foreach($plainData as $chunk)
+        {
+            $partialEncrypted = '';
 
-        if (!$success) {
-            throw new BaseException("OpenSSL: Unable to generate signature");
+            //using for example OPENSSL_PKCS1_PADDING as padding
+            $encryptionOk = openssl_private_encrypt($chunk, $partialEncrypted, $privateKey, OPENSSL_PKCS1_PADDING);
+
+            if($encryptionOk === false){
+                throw new BaseException("OpenSSL: Unable to generate signature");
+            }//also you can return and error. If too big this will be false
+            $encrypted .= $partialEncrypted;
         }
+        return base64_encode($encrypted);//encoding the whole binary String as MIME base 64
 
-        $sign_b64 = base64_encode($signature);
-
-        return $sign_b64;
     }
 
     /**
@@ -45,20 +61,32 @@ class Generator
      */
     public static function parse($licenseKey, $publicKey)
     {
-        $sign = base64_decode($licenseKey);
+
+        $decrypted = '';
+
+        //decode must be done before spliting for getting the binary String
+        $data = str_split(base64_decode($licenseKey), 256);
 
         $key = openssl_pkey_get_public($publicKey);
         if (!$key) {
             throw new BaseException("OpenSSL: Unable to get public key");
         }
 
-        $success = openssl_public_decrypt($sign, $decryptedData, $publicKey);
-        openssl_free_key($key);
+        foreach($data as $chunk)
+        {
+            $partial = '';
 
-        if (!$success) {
-            throw new BaseException("OpenSSL: Unable to generate signature");
+            //be sure to match padding
+            $decryptionOK = openssl_public_decrypt($chunk, $partial, $publicKey, OPENSSL_PKCS1_PADDING);
+
+            if($decryptionOK === false){
+                throw new BaseException("OpenSSL: Unable to parse signature");
+            }//here also processed errors in decryption. If too big this will be false
+            $decrypted .= $partial;
         }
+        $decryptedData = json_decode($decrypted, true);
+        return $decryptedData;
 
-        return json_decode($decryptedData, true);
+
     }
 }
