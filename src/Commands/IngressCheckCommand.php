16ba4e46731c61bfbaff2d62a6a8b7a4c8c5ff57
Bhavik MacBook PRO 16 <bhavik@ingressit.com>
2023-02-01 23:34:04 +0530

    Added Command Console

---
+++ src/Commands/IngressCheckCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace IngressITSolutions\Generator\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use Exception;
+
+class IngressCheckCommand extends Command
+{
+    protected $signature = 'ingress:check';
+
+    protected $description = 'Check the health of Project';
+
+    public function handle(): int
+    {
+        $licMan = new LicMan();
+        $data_check_result = $licMan->checkPersistence();
+        if ($data_check_result === false) {
+            $this->info('There is some issue.');
+        } else {
+            $this->info('Everything looks good.');
+        }
+
+        return 0;
+    }
+}
